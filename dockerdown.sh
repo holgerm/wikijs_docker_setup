92a78c6618e04ca52e23a5935b38193784091734
Holger Mügge <holger@vps2429277.dedi.server-hosting.expert>
2023-08-23 23:39:55 +0200

    Initial wikijs docker commit

---
+++ dockerdown.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ ! -f .secrets_env_rc ]
+then
+	echo "File not found: .secrets_env_rc"
+        echo "  please create file .secrets_env_rc and set all secret environment variables for docker-compose.yml"
+	exit 1
+fi
+
+source ./.secrets_env_rc
+
+docker compose down
+
+exit 0
